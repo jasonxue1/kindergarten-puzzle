d47024f63bda7fea7c279385bc2824f1a0b3a8a5
jasonxue1 <jasonxue1@users.noreply.github.com>
2025-09-12 13:03:04 +0000

    deploy: 79e2d29cb8942478b20b01ba8f37449ec6f73ce6

---
+++ dev/pkg/puzzle_wasm_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const load_puzzle_from_text: (a: number, b: number) => any;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const wasm_bindgen__convert__closures_____invoke__h5c172b1bb05e9905: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hb90d8934ac3ab8cd: (a: number, b: number) => void;
+export const closure51_externref_shim: (a: number, b: number, c: any) => void;
+export const closure155_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
